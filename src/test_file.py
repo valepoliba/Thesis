92f291e6f58fbd4423b1b259d854c6f789f7d9d5
vale06 <v.botta@studenti.poliba.it>
2022-10-30 18:42:53 +0100

    2 test fatti

--- src/test_file.py
+++ src/test_file.py
@@ -1,11 +1,12 @@
 import os, sys
-
-directory = "outputs/drugbank/oLast_NQ_NEW_3"
+import datetime
+"""
+directory = "outputs/drugbank/oLast_NQ_NEW_optimized" + str(datetime.datetime.now()).replace(":", ".").replace(" ", "_")
 if not os.path.exists(directory):
     os.mkdir(directory)
-
+"""
 iteration = 0
-
+directory = "outputs/drugbank/oLast_NQ_NEW_optimized2022-10-30_16.47.12.228172"
 fileopencount = open(directory + "/output_tmp_LCS_" + str(iteration) + ".nt", "r")
 filecount = len(fileopencount.readlines())
 print("Count: ", filecount)
