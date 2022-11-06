f3f7543b76c147abda89c07a0ea6401e39de67f0
vale06 <v.botta@studenti.poliba.it>
2022-11-06 10:44:43 +0100

    funziona l'array

--- src/test_file.py
+++ src/test_file.py
@@ -4,9 +4,26 @@ import datetime
 directory = "outputs/drugbank/oLast_NQ_NEW_optimized" + str(datetime.datetime.now()).replace(":", ".").replace(" ", "_")
 if not os.path.exists(directory):
     os.mkdir(directory)
-"""
+
 iteration = 0
 directory = "outputs/drugbank/oLast_NQ_NEW_optimized2022-10-30_16.47.12.228172"
 fileopencount = open(directory + "/output_tmp_LCS_" + str(iteration) + ".nt", "r")
 filecount = len(fileopencount.readlines())
 print("Count: ", filecount)
+"""
+
+array1 = [0,1]
+array2 = []
+
+print("array1: ", array1)
+print("array2: ", array2)
+
+array2 = array1
+
+print("array1: ", array1)
+print("array2: ", array2)
+
+array1.append(2)
+
+print("array1: ", array1)
+print("array2: ", array2)
