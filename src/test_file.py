c1f9b18cb13b53b98551ebffbbba29e44df743ff
vale06 <v.botta@studenti.poliba.it>
2022-11-06 20:31:47 +0100

    test e fix sul plotting

--- src/test_file.py
+++ src/test_file.py
@@ -11,7 +11,7 @@ fileopencount = open(directory + "/output_tmp_LCS_" + str(iteration) + ".nt", "r
 filecount = len(fileopencount.readlines())
 print("Count: ", filecount)
 """
-
+"""
 array1 = [0,1]
 array2 = []
 
@@ -27,3 +27,12 @@ array1.append(2)
 
 print("array1: ", array1)
 print("array2: ", array2)
+
+"""
+iteration = 0
+filecount = 5
+
+if iteration > 0 or (iteration == 0 and filecount > 1):
+    print("Entro")
+else:
+    print("Non entro")
