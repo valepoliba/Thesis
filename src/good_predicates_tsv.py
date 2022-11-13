b2d78b1d9becea627c49ff9ec08b47735a37bd08
vale06 <v.botta@studenti.poliba.it>
2022-11-13 13:49:49 +0100

    verifica po significativi classe di test

--- src/good_predicates_tsv.py
+++ src/good_predicates_tsv.py
@@ -3,11 +3,13 @@ import csv
 
 allpredicates = pd.read_csv('datasets/drugbank/all_predicates.tsv', sep='\t')
 badpredicates = pd.read_csv('datasets/drugbank/bad_predicates.tsv', sep='\t')
+uninformative = pd.read_csv('datasets/drugbank/uninformative.tsv', sep='\t')
 print("Count allpredicates: ", allpredicates.count())
 print("Count badpredicates: ", badpredicates.count())
+print("Count uninformative: ", uninformative.count())
 #print(badpredicates)
 
 df = pd.concat([allpredicates, badpredicates]).drop_duplicates(keep=False)
 #print(df)
 print("Count: ", df.count())
-df.to_csv('datasets/drugbank/good_predicates.tsv', index=False, quoting=csv.QUOTE_ALL)
+df.to_csv('datasets/drugbank/good_predicates.tsv', index=False)
--- src/test_file.py
+++ src/test_file.py
@@ -11,8 +11,7 @@ directory = "outputs/drugbank/oLast_NQ_NEW_optimized2022-10-30_16.47.12.228172"
 fileopencount = open(directory + "/output_tmp_LCS_" + str(iteration) + ".nt", "r")
 filecount = len(fileopencount.readlines())
 print("Count: ", filecount)
-"""
-"""
+
 array1 = [0,1]
 array2 = []
 
@@ -38,7 +37,7 @@ if iteration > 0 or (iteration == 0 and filecount > 1):
     print("Entro")
 else:
     print("Non entro")
-"""
+
 
 # Creo file good_predicates.tsv
 import pandas as pd
@@ -54,3 +53,46 @@ df = pd.concat([allpredicates, badpredicates]).drop_duplicates(keep=False)
 #print(df)
 print("Count: ", df.count())
 df.to_csv('datasets/drugbank/good_predicates.tsv', index=False, quoting=csv.QUOTE_ALL)
+"""
+
+import rdflib
+import pandas as pd
+from rdflib import Graph, RDF
+from rdflib.term import _is_valid_uri
+from knowledge_graph import *
+import shlex
+"""
+g = Graph()
+g.parse('outputs/drugbank/oLast_NQ_NEW_3/output_tmp_LCS_0.nt')
+
+good_predicates = pd.read_csv('datasets/drugbank/good_predicates.tsv', sep='\t')
+good_predicates = [rdflib.URIRef(x) for x in good_predicates['good_predicates']]
+
+path = 'outputs/drugbank/oLast_NQ_NEW_3/output_tmp_LCS_0.nt'
+
+for s, p, o in g:
+    if p in good_predicates:
+        if _is_valid_uri(p):
+            print(p)
+"""
+
+with open('datasets/drugbank/good_predicates.tsv', 'r') as good_predicates:
+    gp = good_predicates.read().split()
+outputnt = open('outputs/drugbank/oLast_NQ_NEW_3/output_tmp_LCS_0.nt', 'r')
+outputietration =  open('outputs/drugbank/oLast_NQ_NEW_3/output_tmp_LCS_0_po_significant.nt', 'a')
+filecount = 0
+for line in outputnt.readlines():
+        line = line.replace('<', '').replace('>', '')
+        s, p, o, _ = shlex.split(line)
+        if not ('_:blank_' in o) and not ('_:blank_' in p):
+            for goodline in gp:
+                if goodline == p:
+                    outputietration.write(p + '\n')
+                    filecount += 1
+
+print("Count row: ", filecount)
+
+outputietration.close()
+outputnt.close()
+good_predicates.close()    
+
